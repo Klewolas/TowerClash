c637363149267b539aea0a50823034dcd5f1811e
Berke Altıntaş <berke.altintas@hotmail.com>
2024-11-07 23:30:14 +0300

    Turret targeting.

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,10 +1,58 @@
+using System;
 using UnityEditor;
 using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
     [SerializeField] private float _targetingRange;
+    [SerializeField] private LayerMask _enemyMask;
+    [SerializeField] private Transform _turretRotationPoint;
+
+    private Transform _target;
     
+    //TODO : They will convert to UniTask.
+    private void Update()
+    {
+        if (_target == null)
+        {
+            FindTarget();
+            return;
+        }
+
+        RotateTowardsTarget();
+
+        if (!CheckTargetIsInRange())
+        {
+            _target = null;
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        float angle =
+            Mathf.Atan2(_target.position.y - transform.position.y, _target.position.x - transform.position.x) *
+            Mathf.Rad2Deg - 90f;
+        
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f,0f,angle));
+        _turretRotationPoint.rotation = targetRotation;
+    }
+    
+    private bool CheckTargetIsInRange()
+    {
+        return Vector2.Distance(_target.position, transform.position) <= _targetingRange;
+    }
+
+    private void FindTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, _targetingRange, (Vector2)transform.position,
+            0f, _enemyMask);
+
+        if (hits.Length > 0)
+        {
+            _target = hits[0].transform;
+        }
+    }
+
     private void OnDrawGizmosSelected()
     {
         Handles.color = Color.cyan;
